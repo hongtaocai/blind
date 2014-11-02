dd111886e02f320f99e7e130147c24274d99901a
hcai <hcai@twitter.com>
2014-11-02 12:41:19 -0800

    stock card color

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,7 +8,13 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .factory('stocks', function(){
+  .constant('config', {
+    'darkGreen' : '#0AA344',
+    'lightGreen': '#BDDD22',
+    'darkRed' : '#FF2121',
+    'lightRed' : '#FFB3A7'
+  })
+  .service('stocks', function(){
     function randomString(length) {
       var result = '';
       var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -17,6 +23,7 @@ angular.module('clientApp')
       }
       return result;
     }
+
     var stocks = [];
     for (var i = 0; i < 52 ; i++ ) {
       stocks.push({
@@ -25,19 +32,46 @@ angular.module('clientApp')
         'nextMinPredictedGrowth': Math.random()*2 -1,
         'lastMinActualGrowth': Math.random()*2 -1,
         'errorRate': Math.random()*100
-      });
-    }
-    return stocks;
+      });}
+    return {
+      stocks: stocks
+    };
   })
-  .controller('MainCtrl', function ($scope, stocks) {
-    $scope.searchText = '';
-    $scope.stocks = stocks;
+  .controller('MainCtrl', function ($scope, stocks, config) {
+
+    (function(){
+      $scope.searchText = '';
+      $scope.stocks = stocks.stocks;
+      $scope.maxNextMinPredictedGrowth = - Infinity;
+      $scope.minNextMinPredictedGrowth = Infinity;
+      for (var stockIndex in $scope.stocks) {
+        if ($scope.maxNextMinPredictedGrowth <  $scope.stocks[stockIndex].nextMinPredictedGrowth) {
+          $scope.maxNextMinPredictedGrowth =   $scope.stocks[stockIndex].nextMinPredictedGrowth;
+        }
+        if ($scope.minNextMinPredictedGrowth >  $scope.stocks[stockIndex].nextMinPredictedGrowth) {
+          $scope.minNextMinPredictedGrowth =  $scope.stocks[stockIndex].nextMinPredictedGrowth;
+        }
+      }
+    })();
+
     $scope.formatGrowth = function(growth) {
       var prefix = growth >= 0 ? '+' : '';
       return prefix + growth.toFixed(4);
     };
+
     $scope.formatErrorRate = function(errorRate) {
       return Math.floor(errorRate);
+    };
+
+    $scope.getColorFromGrowth = function(growth, maxGrowth, minGrowth) {
+      var scale = null;
+      if (growth >= 0) {
+        scale = chroma.scale([config.lightGreen, config.darkGreen]);
+        return scale(growth/maxGrowth).hex();
+      } else {
+        scale = chroma.scale([config.lightRed, config.darkRed]);
+        return scale(growth/minGrowth).hex();
+      }
     };
   });
 
