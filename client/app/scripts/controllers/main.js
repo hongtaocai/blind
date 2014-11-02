5191353b51193b709a1fda3e66084858b7ed86bc
hcai <hcai@twitter.com>
2014-11-02 01:56:05 -0800

    add stocks

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,10 +8,33 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .factory('stocks', function(){
+    function randomString(length) {
+      var result = '';
+      var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      for (var i = length; i > 0; --i) {
+        result += chars[Math.round(Math.random() * (chars.length - 1))];
+      }
+      return result;
+    }
+    function formatGrowth(growth) {
+      var prefix = growth >= 0 ? '+' : '-';
+      return prefix + growth;
+    }
+    var stocks = [];
+    for (var i = 0; i < 50 ; i++ ) {
+      stocks.push({
+        'name': randomString(4),
+        'lastMinRealGrowth': formatGrowth(Math.random().toFixed(4)*2 -1),
+        'nextMinPredictedGrowth': formatGrowth(Math.random().toFixed(4)),
+        'lastMinPredictedGrowth': formatGrowth(Math.random().toFixed(4)),
+        'errorRate': Math.floor(Math.random()*100)
+      });
+    }
+    return stocks;
+  })
+  .controller('MainCtrl', function ($scope, stocks) {
+    $scope.searchText = '';
+    $scope.stocks = stocks;
   });
+
